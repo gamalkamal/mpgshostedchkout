b94137e21cfb7241386f25bf56d2b62bd823a4ee
vikas-saini07 <vikas-saini@live.com>
2020-05-27 20:47:00 -0500

    index file moved

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<html>
+    <body>
+        <h1>MPGS TESTS</h1>
+        <ul>
+            <li><a href="./hostedchkout/welcome.php">Hosted Checkout using MPGS</a></li>
+        </ul>
+    </body>
+</html>
