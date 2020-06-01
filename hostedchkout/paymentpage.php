7bf9ad47a04935456a1c8be6687dcc50534f2fd2
vikas-saini07 <56854067+vikas-saini07@users.noreply.github.com>
2020-06-01 16:48:54 +1000

    Update paymentpage.php

--- hostedchkout/paymentpage.php
+++ hostedchkout/paymentpage.php
@@ -9,12 +9,12 @@ $cd = $name = $amount = $description = $phone = $Email = "";
 
 <form class="form" action="session.php" method="post">
 <ul class="form-style">
-<li><label>Merchant Name<span class="required">*</span></label><input type="text" name="name" value="TESTVIKAS"></li>
+<li><label>Merchant Name<span class="required">*</span></label><input type="text" name="name" value=""></li>
 <li><label>Amount<span class="required">*</span></label><input type="number" name="amount" value=1></li>
 <li><label>Description <span class="required">*</span></label><input type="text" name="description" value="Sun Glasses"></li>
 <li><label>Country code<span class="required">*</span></label><input type="number" name="cd" value=61></li>
-<li><label>Phone<span class="required">*</span></label><input type="number" name="phone" value=468372014></li>
-<li><label>Email<span class="required">*</span></label><input type="text" name="Email" value="vikas.saini@mastercard.com"></li>
+<li><label>Phone<span class="required">*</span></label><input type="number" name="phone" value=""></li>
+<li><label>Email<span class="required">*</span></label><input type="text" name="Email" value=""></li>
 <li><input type="submit" value="Checkout"></li>
 </ul>
 </form>
