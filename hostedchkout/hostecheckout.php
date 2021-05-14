85f3cf521142a98793f690b5e95e016e17052238
vikas-saini07 <56854067+vikas-saini07@users.noreply.github.com>
2021-05-14 11:24:03 +1000

    Update hostecheckout.php

--- hostedchkout/hostecheckout.php
+++ hostedchkout/hostecheckout.php
@@ -11,11 +11,15 @@ $description = $_POST['description'];
 $phone = $_POST['phone'];
 $Email = $_POST['Email'];
 $order_id = date("YmdHis");
+$txn_id = date("YYmdHis");
+$currency = $_POST['currency'];
+$order_ref = date("YYmdHis");
 //$order_id = $merchant . '-' . date("YmdHis");
 //$order_id = $merchant . '-' . uniqid(rand(), true) . date("YmdHis");
-$comurl = "https://mpgstests.herokuapp.com/hostedchkout/complete.html";
+$comurl = "https://ech-10-168-129-136.mastercard.int/mpgstest/hostedchkout/complete.html";
 //Enter interaction.returnUrl below
 $url = $rwurl;
+
 $apiUsername = "merchant.$merchant";
 
 $curl = curl_init();
@@ -28,9 +32,9 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$apiUsername&apiPassword=$apipassword&order.currency=AUD&order.id=$order_id&order.amount=$amount&interaction.operation=PURCHASE",
-//uncomment below if you want to use interaction.returnUrl and then comment above
-  //CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$apiUsername&apiPassword=$apipassword&order.currency=AUD&order.id=$order_id&order.amount=$amount&interaction.returnUrl=$url&interaction.operation=PURCHASE",
+ //CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$apiUsername&apiPassword=$apipassword&order.currency=$currency&order.id=$order_id&order.amount=$amount&interaction.operation=PURCHASE&interaction.returnUrl=$rwurl",
+//CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$apiUsername&apiPassword=$apipassword&order.currency=$currency&order.id=$order_id&order.amount=$amount&interaction.operation=PURCHASE&interaction.returnUrl=$rwurl&interaction.merchant.url=$rwurl&interaction.merchant.name=$merchant",
+CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$apiUsername&apiPassword=$apipassword&order.currency=$currency&order.id=$order_id&order.amount=$amount&interaction.operation=PURCHASE&interaction.merchant.url=$rwurl&interaction.merchant.name=$merchant&interaction.timeout=600",
   CURLOPT_HTTPHEADER => array(
     "Content-Type: application/x-www-form-urlencoded",
     "cache-control: no-cache")
@@ -44,131 +48,154 @@ curl_close($curl);
 
 <html>
  <link rel="stylesheet" type="text/css" href="paystyle.css">
-
     <head>
-        <script src = "https://test-gateway.mastercard.com/checkout/version/56/checkout.js"
+<meta charset="utf-8">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="  crossorigin="anonymous"></script>
+        <script src="<?php echo $jsurl ?>"
+                data-error="errorCallback"
                 data-complete="completeCallback"
-                data-error="<?php echo $url ?>"
-                data-cancel="<?php echo $url ?>"
-        	data-beforeRedirect="beforeRedirect"
-        	data-afterRedirect="afterRedirect"
-                data-timeout="<?php echo $url ?>">
+                data-cancel="cancelCallback">
         </script>
 
         <script type="text/javascript">
-	var params = "<?php echo $response ?>";
-	var elements =  params.split('&');
-	var keyValues = {};
-	for(var i in elements) {
-    		var key = elements[i].split("=");
-    			if (key.length > 1) {
-     				 keyValues[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
-   					 }
-	}
+                //data-complete="completeCallback">
+                //data-beforeRedirect="beforeRedirect"
+                //data-afterRedirect="afterRedirect"
+                //data-error="https://ech-10-168-129-136.mastercard.int/mpgstest/hostedchkout/error.html"
+                //data-cancel="https://ech-10-168-129-136.mastercard.int/mpgstest/hostedchkout/cancel.html">
+        var params = "<?php echo $response ?>";
+        var elements =  params.split('&');
+        var keyValues = {};
+        for(var i in elements) {
+                var key = elements[i].split("=");
+                        if (key.length > 1) {
+                                 keyValues[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
+                                         }
+        }
 
-	var url = "<?php echo $url ?>";
-	var comurl = "<?php echo $comurl ?>";
-	var successIndicator = keyValues["successIndicator"];
-	var sessionVersion = keyValues["session.version"];
-	var sessionId = keyValues["session.id"];
-	var merchantID = keyValues["merchant"];
-	var amountID = "<?php echo $amount ?>";
-	var descriptionID = "<?php echo $description ?>";
-	var orderID = "<?php echo $order_id ?>";
-	var emailID = "<?php echo $Email ?>";
-	var phoneID = "<?php echo $phone ?>";
-	var merchant = "<?php echo $merchant ?>";
-	var resultIndicator = null;
-       	  function beforeRedirect() {
-        	return {
-            	successIndicator: successIndicator,
-            	orderId: orderID,
-            	sessionId: sessionId,
-            	sessionVersion:sessionVersion ,
-            	merchantId: merchantID,
-            	url: url,
-            	comurl: comurl
-        	};
-    	   }
-	   function afterRedirect(data) {
-		  console.log(data);
-		  console.log(resultIndicator);
-		  console.log(data.successIndicator);
-	        // Compare with the resultIndicator saved in the completeCallback() method
-       		 if (resultIndicator) {
-        	  var result = (resultIndicator === data.successIndicator) ? "SUCCESS" : "ERROR";
-		  //console.log(data.orderId);
-     	       	  window.location.href = comurl;
-		  //window.location.href = "/mpgstest/hostedchkout/" + data.orderId + "/" + result;
-        	}	
-        	else {
-		  successIndicator = data.successIndicator;
-            	  orderId = data.orderId;
-                  sessionId = data.sessionId;
-                  sessionVersion = data.sessionVersion;
-                  merchantId = data.merchantId;
-            	  window.location.href = comurl;
-		  //window.location.href = "/mpgstest/hostedchkout/" + data.orderId + "/" + data.successIndicator + "/" + data.sessionId;
-      	    }}
+        var url = "<?php echo $url ?>";
+        var comurl = "<?php echo $comurl ?>";
+        var successIndicator = keyValues["successIndicator"];
+        var sessionVersion = keyValues["session.version"];
+        var sessionId = keyValues["session.id"];
+console.log(sessionId);
+        var merchantID = keyValues["merchant"];
+        var amountID = "<?php echo $amount ?>";
+        var descriptionID = "<?php echo $description ?>";
+        var orderID = "<?php echo $order_id ?>";
+        var orderref = "<?php echo $order_ref ?>";
+        var transactionId = "<?php echo $txn_id ?>";
+        var emailID = "<?php echo $Email ?>";
+        var phoneID = "<?php echo $phone ?>";
+        var merchant = "<?php echo $merchant ?>";
+        var currency = "<?php echo $currency ?>";
+        var resultIndicator = null;
+console.log(url);
+          function beforeRedirect() {
+                console.log(orderID);
+                console.log("before redirect");
+                return {
+                        successIndicator: "successIndicator",
+                };
+           }
+           function afterRedirect(data) {
+                  //console.log("after redirect");
+                  //console.log(resultIndicator);
+                  //console.log(data.successIndicator);
+                // Compare with the resultIndicator saved in the completeCallback() method
+                  window.location.href = "https://www.google.com";
+            }
+            function completeCallback(resultIndicator, sessionVersion) {
+                         console.log('completeCallback');
+                //       alert("Payment Success!!");
+                  // Save the resultIndicator
+        //        resultIndicator = response;
+        //          var result = (resultIndicator === successIndicator) ? "SUCCESS" : "ERROR";
+                  //window.location.href = "/mpgstest/hostedchkout/" + orderId + "/" + result;
+        //        window.location.href = comurl;
+                 // window.location.href = "https://ech-10-168-129-136.mastercard.int/mpgstest/hostedsession/3dslib/response.php";
+/*
+                 var receiptdata = {
+                        merchant: "TESTMPGSVIKAS",
+                        apipassword: "8b1e70742af706873163399ac5eccbe8",
+                        orderid: orderId
+                        };
 
-    	    function completeCallback(response) {
-    		  // Save the resultIndicator
-        	  resultIndicator = response;
-                  var result = (resultIndicator === successIndicator) ? "SUCCESS" : "ERROR";
-                 // window.location.href = url;
-		  window.location.href = comurl;
+                $.post('receipt.php', receiptdata, function (result, status) {
+                        console.log(result);
+                        });
+*/
+            }
+
+            function timeoutCallback() {
+                  console.log('Timeout');
             }
 
             function errorCallback(error) {
                   console.log(JSON.stringify(error));
-		  console.log('sessionId');
+                  console.log('sessionId');
             }
             function cancelCallback() {
                   console.log('Payment cancelled');
             }
 
 
-            function timeoutCallback() {
-                  console.log('Timeout');
-            }
 
 console.log(orderID);
 console.log(sessionId);
             Checkout.configure({
-		session: { 
-            		id: sessionId
-       			},
+                session: {
+                        id: sessionId
+                        },
                 merchant: merchantID,
                 order: {
                     amount: amountID,
-                    currency: 'AUD',
+                    currency: currency,
                     description: descriptionID,
-                    id: orderID
+                    id: orderID,
+                    reference: orderref,
+                    item:{
+                          name: "E99994901TEST",
+                          quantity: "1",
+                          unitPrice: "0.02",
+                        },
+                    item:{
+                          name: "E99994901TEST1",
+                          quantity: "2",
+                          unitPrice: "0.03",
+                        }
                 },
-		customer: {
-			email: emailID,
-			phone: phoneID
-		},
+/*                      transaction     : {
+                                acquirer : {
+                                        id : transactionId
+                                }
+                        },
+*/              customer: {
+                        email: emailID,
+                        phone: phoneID
+                },
                 interaction: {
-                    operation: 'PURCHASE', // set this field to 'PURCHASE' for Hosted Checkout to perform a Pay Operation.
+                    operation: 'PURCHASE',
+                    country: 'AUS',
+        //            operation: 'PURCHASE', // set this field to 'PURCHASE' for Hosted Checkout to perform a Pay Operation.
                     merchant: {
                         name: merchant,
                         address: {
                             line1: 'Test',
                             line2: 'Test'
-       		                 }
-                	      },
-		    displayControl: {
-   		        orderSummary    	: 'HIDE',
-            		paymentConfirmation	: 'HIDE',
-            		billingAddress 		: 'HIDE',
-         		customerEmail   	: 'HIDE',
-         		shipping        	: 'HIDE'
-        			   }
-        	          }
+                                 },
+                        url: 'https://ech-10-168-129-136.mastercard.int'
+                              },
+                    displayControl: {
+                        orderSummary            : 'HIDE',
+                        paymentConfirmation     : 'HIDE',
+                        billingAddress          : 'HIDE',
+                        customerEmail           : 'HIDE',
+                        shipping                : 'HIDE'
+                                   }
+                          }
             });
         </script>
-    </head>
     <body>
     <div class=box>
     <h2>Review your details</h2>
@@ -177,12 +204,11 @@ console.log(sessionId);
     <p>Phone: <?php echo $phone ?></p>
     <p>Email: <?php echo $Email ?></p>
     <p>Description: <?php echo $description ?></p>
-    <h1><input type="button" value="Pay" onclick="Checkout.showLightbox();" /></h1>
-    <h1><input type="button" value="Pay with Payment Page" onclick="Checkout.showPaymentPage();" /></h1>
+<input type="button" value="Pay with Lightbox" onclick="Checkout.showLightbox();" />
+      <input type="button" value="Pay with Payment Page" onclick="Checkout.showPaymentPage();" />
     </div>
 <!--        <input type="button" value="Pay with Lightbox" onclick="Checkout.showLightbox();" />
         <input type="button" value="Pay with Payment Page" onclick="Checkout.showPaymentPage();" /> -->
 
     </body>
 </html>
-
